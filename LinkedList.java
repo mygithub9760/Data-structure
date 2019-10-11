53372fdff4fecf41fd9c2d4ebe46a8b105e191a5
mygithub9760 <39545240+mygithub9760@users.noreply.github.com>
2019-10-11 15:32:56 +0530

    Create LinkedList.java

---
+++ LinkedList.java
@@ -0,0 +1,129 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.*;
+import java.io.*;
+public class Main
+{
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		list.insert(1);
+		list.insert(2);
+		list.insert(4);
+		list.insert(9);
+		//System.out.println("First List");
+		//list.print();
+		LinkedList list2 = new LinkedList();
+		list2.insert(10);
+		list2.insert(20);
+		list2.insert(30);
+		list2.insert(40);
+		//System.out.println();
+		//System.out.println("Second List");
+		//list2.print();
+		
+		//System.out.println(list.find(2));
+		System.out.println("Before reversing");
+		list.print();
+		list.reverse();
+		System.out.println("After reversing");
+		list.print();
+		System.out.println(list.isCircular());
+		
+	}
+}
+
+class LinkedList
+{
+    Node head;
+    
+    class Node
+    {
+        int data;
+        Node next;
+        
+        Node(int data)
+        {
+            this.data = data;
+            next = null;
+        }
+    }
+    
+    public void insert(int data)
+    {
+        Node node = new Node(data);
+        
+        if(this.head == null)
+        {
+            this.head = node;
+        }
+        else
+        {
+            Node temp = this.head;
+            while(temp.next != null)
+            {
+                temp = temp.next;
+            }
+            temp.next = node;
+        }
+    }
+    
+    public boolean find(int key)
+    {
+        if(this.head == null)
+            return false;
+        Node temp = this.head;
+        while(temp != null)
+        {
+            if(temp.data == key)
+                return true;
+            temp = temp.next;
+        }
+        return false;
+    }
+    
+    public void reverse()
+    {
+        Node prev = null;
+        Node nextCurrent = null;
+        //Node current = head;
+        while(this.head!=null)
+        {
+            nextCurrent = this.head.next;
+            this.head.next = prev;
+            prev = this.head;
+            this.head = nextCurrent;
+        }
+        this.head = prev;
+    }
+    
+    
+    public boolean isCircular()
+    {
+        Node fast = this.head;
+        Node slow = this.head;
+        while(slow!=null && fast!=null && fast.next!=null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+            
+            if(fast == slow)
+                return true;
+        }
+        return false;
+    }
+    
+    public void print()
+    {
+        Node temp = this.head;
+        while(temp != null)
+        {
+            System.out.println(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
