4725a41d60505610e6de302b00f33620419a40e4
mygithub9760 <39545240+mygithub9760@users.noreply.github.com>
2019-10-18 20:32:03 +0530

    Update Trie.java

--- Trie.java
+++ Trie.java
@@ -15,7 +15,13 @@ public class Main
 		t.insert("bcd");
 		t.insert("abcd");
 		t.insert("ab");
-		System.out.println(t.search("abced"));
+		System.out.println(t.count);
+		t.delete("abc");
+		t.delete("abcd");
+		t.delete("ab");
+
+		//t.insert("abc");
+		System.out.println(t.search("ab"));
 	}
 }
 
@@ -61,6 +67,7 @@ class Trie
         }
         
         temp.eow = true;
+        count++;
     }
     
     public boolean search(String str)
@@ -84,14 +91,48 @@ class Trie
         return temp.eow;
     }
     
+    
+    public void delete(String str)
+    {
+        TrieNode temp = root;
+        int size = str.length();
+        Stack<TrieNode> s1 = new Stack<>();
+        Stack<Character> s2 = new Stack<>();
+        boolean flag = true;
+        for(int i =0;i<size;i++)
+        {
+            char ch = str.charAt(i);
+            if(!temp.map.containsKey(ch))
+            {
+                flag = false;
+                break;
+            }
+            s1.push(temp);
+            s2.push(ch);
+            temp = temp.map.get(ch);
+            
+        }
+        
+        if(!flag || !temp.eow){
+            System.out.println("string " + str + " not found");
+            return;
+        }
+        temp.eow = false;
+        while(!s1.empty() && !s2.empty())
+        {
+            TrieNode t = s1.pop();
+            char ch = s2.pop();
+            if(!t.map.get(ch).map.isEmpty() || t.map.get(ch).eow)
+            {
+                break;
+            }
+            t.map.remove(ch);
+        }
+        System.out.println("string " + str + " deleted successfully.");
+        count--;
+        
+    }
+    
+    
+    
 }
-
-
-
-
-
-
-
-
-
-
